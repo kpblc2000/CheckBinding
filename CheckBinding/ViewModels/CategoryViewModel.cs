7252e2d4af4685e5ba68cff46ec8245a2c364b13
Aleksey Kulik <kpblc2000@gmail.com>
2021-05-01 23:09:31 +0300

    Связка View - ViewModel

--- CheckBinding/ViewModels/CategoryViewModel.cs
+++ CheckBinding/ViewModels/CategoryViewModel.cs
@@ -2,6 +2,7 @@
 using CheckBinding.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,17 @@ namespace CheckBinding.ViewModels
     public class CategoryViewModel : ViewModel
     {
 
-        private List<Category> _Caterories;
+        public CategoryViewModel()
+        {
+            var lst = Enumerable.Range(1, 10)
+                .Select(i => new Category
+                {
+                    Id = i,
+                    Name = $"Caterogy{i}"
+                });
+            Categories = new List<Category>(lst);
+        }
+
         public List<Category> Categories
         {
             get { return _Caterories; }
@@ -22,10 +33,30 @@ namespace CheckBinding.ViewModels
             }
         }
 
-        public CategoryViewModel()
+        public Category SelectedCategoryComboBox
         {
-
+            get { return _SelectedCategoryComboBox; }
+            set
+            {
+                _SelectedCategoryComboBox = value;
+                OnPropertyChanged(nameof(SelectedCategoryComboBox));
+            }
         }
+
+        public Category SelectedCategoryListBox
+        {
+            get { return _SelectedCategoryListBox; }
+            set
+            {
+                _SelectedCategoryListBox = value;
+                OnPropertyChanged(nameof(SelectedCategoryListBox));
+            }
+        }
+
+
+        private List<Category> _Caterories;
+        private Category _SelectedCategoryComboBox;
+        private Category _SelectedCategoryListBox;
 
     }
 }
